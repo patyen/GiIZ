506105fb64d8eef84c6104bc1de67889543423a0
Daniel Piskorski <danpisq@gmail.com>
2018-03-21 00:46:35 +0100

    Zest2 Zad2 Randomizacja grafu

---
+++ Set2/Task2/README.md
@@ -0,0 +1,14 @@
+Prosty skrypt od randomizacji grafow.
+Zestaw1 Zadanie 2
+
+wymagania:
+        -python
+
+użycie:
+        python rand_graph.py <NazwaPliku.txt>
+
+Zakładamy że plik w którym zapisany jest graf to lista incydencji, w której kolejne wiersze odpowiadają kolejnym krawędziom grafu.
+
+Autor: Daniel Piskorski
+21.03.2018
+
---
+++ Set2/Task2/rand_graph.py
@@ -0,0 +1,61 @@
+import random
+import sys
+
+file = open(sys.argv[1], "r")
+
+data = [i.split() for i in file  if i != 'MI\n']
+
+flag = 1;
+data_size = len(data)
+if(data_size) > 2:
+    while flag:
+        first_index = random.choice(range(len(data)))
+        second_index = random.choice(range(len(data)))
+        while first_index == second_index:
+            second_index = random.choice(range(len(data)))
+
+        tmp1 = data[first_index]
+        tmp2 = data[second_index]
+
+        back1 = tmp1[:]
+        back2 = tmp2[:]
+        
+        if(first_index < second_index):
+            del data[second_index]
+            del data[first_index]
+        else:
+            del data[first_index]
+            del data[second_index]
+    
+        zipped = zip(tmp1, tmp2)
+        flag = 0
+        if ('1','1') in zipped:
+            flag = 1
+
+        n_index_one = tmp1.index('1')
+        n_index_two = tmp2.index('1')
+
+        tmp1[n_index_one] = '0'
+        tmp2[n_index_two] = '0'
+        
+        tmp1[n_index_two] = '1'
+        tmp2[n_index_one] = '1'
+
+        if tmp1 in data or tmp2 in data:
+            flag = 1
+
+        data.append(back1)
+        data.append(back2)
+                    
+data.append(tmp1)
+data.append(tmp2)
+
+tosave = open("result.txt",'w')
+
+tosave.write('MI\n')
+for el in data:
+    for i, num  in enumerate(el):
+        tosave.write(str(num))
+        if i != len(el) - 1:
+            tosave.write(' ')
+    tosave.write('\n')
