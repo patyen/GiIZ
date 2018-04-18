28e05e87b6a54a04d161636befb39e06d5e56df1
Daniel Piskorski <danpisq@gmail.com>
2018-04-18 19:37:26 +0200

    adding labels

--- Set1/Task2/draw_graph.py
+++ Set1/Task2/draw_graph.py
@@ -8,12 +8,16 @@ data = [i.split() for i in file  if i != 'MI\n']
 r = 1
 x = []
 y = []
+text_x = []
+text_y = []
 
 #calculating points coordinances
 for i in range(len(data[0])):
     alfa = 2 * m.pi * i / len(data[0])
     x.append(r * m.cos(alfa))
     y.append(r * m.sin(alfa))
+    text_x.append(1.2*r * m.cos(alfa))
+    text_y.append(1.2*r * m.sin(alfa))
 
 x = np.array(x)
 y = np.array(y)
@@ -29,6 +33,10 @@ for record in data:
             ys.append(y[i])
     plt.plot(xs,ys, c='b')
 print(lines)
+
 #plotting
 plt.scatter(x,y, c='r')
+plt.axis('off')
+for i in range(len(x)):
+    plt.text(text_x[i],text_y[i], str(i+1))
 plt.show()
