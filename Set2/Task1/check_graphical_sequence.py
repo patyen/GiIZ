76c08715e7c34ed5c2008567753ead81f6ce67e2
Pawlllosss <oczadlypawe@gmail.com>
2018-05-08 13:53:45 +0200

    Slight improvements to Task1 and Task 4 - set 2

--- Set2/Task1/check_graphical_sequence.py
+++ Set2/Task1/check_graphical_sequence.py
@@ -1,3 +1,40 @@
+def createAdjacencyMatrix(sequence):
+    sequence = sorted(sequence, reverse=True)
+    vertices = len(sequence)
+
+    #print(sequence)
+
+    sequenceNodes = [[nodeNmb, degree] for nodeNmb, degree in enumerate(sequence)]
+    #[[node1,connections1],...,[noden,connectionsn]]
+    #print(sequenceNodes)
+
+    adjacencyMatrix = []
+
+    #first generate an empty matrix
+    for i in range(vertices):
+        adjacencyMatrix.append([0] * vertices)
+
+
+    #while there are still some connections possible (it's sorted so we can take first element)
+    while sequenceNodes[0][1] > 0:
+        connectionNumber = sequenceNodes[0][1]
+        sequenceNodes[0][1] = 0#zero current node connection
+        neighbourOffset = 1
+        while connectionNumber > 0:
+            if sequenceNodes[neighbourOffset][1] > 0:
+                currentNode = sequenceNodes[0][0]#number of current node
+                currentNeighbour = sequenceNodes[neighbourOffset][0]#number of node with which we currently create a connection
+                adjacencyMatrix[currentNode][currentNeighbour] = 1
+                adjacencyMatrix[currentNeighbour][currentNode] = 1
+                sequenceNodes[neighbourOffset][1] -= 1
+                connectionNumber -= 1
+
+            neighbourOffset += 1
+
+        sequenceNodes = sorted(sequenceNodes, key=lambda l: l[1], reverse=True)
+
+    return adjacencyMatrix
+
 def calculateSequence(sequence):
 
     #usuwać zera
@@ -46,5 +83,23 @@ if __name__  == "__main__":
 
     if calculateSequence(sequence):
         print("Jest ciagiem graficznym")
+        adjacencyMatrix = createAdjacencyMatrix(sequence)
+        print(adjacencyMatrix)
+
+        #creating incedency matrix
+        tmp = []
+        matrix_length = len(adjacencyMatrix)
+
+        for col_nmb in range(matrix_length):
+            for row_nmb in range(col_nmb, matrix_length):
+                if (adjacencyMatrix[row_nmb][col_nmb]):  # jeżeli znaleziono krawędź
+                    tmp.append([1 if (i == row_nmb or i == col_nmb) else 0 for i in range(matrix_length)])
+        #write it to a file
+        output = open("matrix.out", "w")
+
+        print("MI", file = output)
+        for vertex in range(len(tmp)):
+            print(*tmp[vertex], file = output)
+
     else:
         print("Nie jest ciagiem graficznym")
